dcb02fd3226259f76a1c405f43d1f665ab942750
Bill Dailey <bill@mflit.com>
2022-02-05 14:33:13 -0500

    init first interview

--- InterviewTool/interview/admin.py
+++ InterviewTool/interview/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Interview
 
 # Register your models here.
+
+
+class InterviewAdminInline(admin.ModelAdmin):
+    module = Interview
+
+
+admin.site.register(Interview, InterviewAdminInline)
--- InterviewTool/interview/models.py
+++ InterviewTool/interview/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from accounttree.models import Account
 
 # Create your models here.
+
+
+class Interview(models.Model):
+    interviewee = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True)
+    # interviewer = models.ForeignKey(Account, on_delete=models.CASCADE())
+    title = models.CharField(max_length=100)
+    note = models.CharField(max_length=1024)
+    active = models.BooleanField(default=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
